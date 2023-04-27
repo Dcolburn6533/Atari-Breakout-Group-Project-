9f35e9630bf287dfb724f289d3039859e4ff8a16
Dcolb <Dcolb@66.71.126.4>
2023-04-27 14:37:50 -0400

    First building project

--- Atari Breakout Project/src/AtariBreakout/TestMain.java
+++ Atari Breakout Project/src/AtariBreakout/TestMain.java
@@ -16,11 +16,23 @@ public class TestMain {
          Score s = new Score();
          c.SetRadius(5);
          c.SetPosition(240, 120);
-         
-         r.SetXPos(120);
+         c.SetXVelocity(10);
+         c.SetYVelocity(20);
+         r.SetXPos(220);
          r.SetYPos(120);
          r.SetLength(60);
          if(Col.collide(c,r )){
+            System.out.println("You are not a fucking idiot");
+            s.addScore();
+            System.out.println("you have earned " + s.GetScore() + " points");
+         }
+         else{
+             System.out.println("You are not a fucking idiot");
+             System.out.println("you have earned " + s.GetScore() + " points");
+         }
+         
+         c.SetPosition();
+          if(Col.collide(c,r )){
             System.out.println("You are not a fucking idiot");
             s.addScore();
             System.out.println("you have earned " + s.GetScore() + " points");
