7cd87f9d5ce279bfe571203e25c17b5319a7b1ec
Dcolb <Dcolb@66.71.126.4>
2023-05-01 12:44:09 -0400

    First building project

--- Atari Breakout Project/src/AtariBreakout/LaunchGui.java
+++ Atari Breakout Project/src/AtariBreakout/LaunchGui.java
@@ -5,6 +5,7 @@
 package AtariBreakout;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 /**
  *
  * @author Dcolb
@@ -28,7 +29,7 @@ public class LaunchGui extends JFrame {
         JPanel panel1 = new JPanel(new GridLayout(3,1));
         
         JPanel panel2 = new JPanel();
-         JPanel panel4 = new JPanel();
+        JPanel panel4 = new JPanel();
         JPanel panel3 = new JPanel(new GridLayout(1,3));
         panel2.setSize(600, 200);
         panel3.setSize(600, 100);
@@ -45,6 +46,65 @@ public class LaunchGui extends JFrame {
          this.add(panel1);
          this.setForeground(Color.red);
          
+         
+         JPanel GamePanel = new JPanel(new GridLayout( 10,    1));
+         JPanel GamePanel1 = new JPanel(new GridLayout(1,8));
+         JPanel GamePanel2 = new JPanel(new GridLayout(1,8));
+         JPanel GamePanel3 = new JPanel(new GridLayout(1,8));
+         JPanel GamePanel4 = new JPanel(new GridLayout(1,8));
+         JPanel GamePanel5 = new JPanel(new GridLayout(1,8));
+         JPanel PlayArea = new JPanel();
+         GamePanel1.setSize(600, 50);
+         GamePanel2.setSize(600, 50);
+         GamePanel3.setSize(600, 50);
+         GamePanel4.setSize(600, 50);
+         GamePanel5.setSize(600, 50);
+         PlayArea.setSize(600, 350);
+         JLabel Rectangles[] = new JLabel[40];
+         Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+         for (int i = 0; i<8; i++){
+             Rectangles[i] = new JLabel(new ImageIcon("C:\\Users\\Dcolb\\Documents\\NetBeansProjects\\Atari-Breakout-Group-Project-\\Atari Breakout Project\\src\\AtariBreakout\\Red.png"));
+             Rectangles[i].setSize(75, 50);
+             
+             Rectangles[i].setBorder(blackline);
+             GamePanel1.add(Rectangles[i]);
+         }
+         for (int i = 8; i<16; i++){
+             Rectangles[i] = new JLabel(new ImageIcon("C:\\Users\\Dcolb\\Documents\\NetBeansProjects\\Atari-Breakout-Group-Project-\\Atari Breakout Project\\src\\AtariBreakout\\Orange.png"));
+             Rectangles[i].setSize(75, 50);
+             Rectangles[i].setBorder(blackline);
+             GamePanel2.add(Rectangles[i]);
+         }
+         for (int i = 16; i<24; i++){
+             Rectangles[i] = new JLabel(new ImageIcon("C:\\Users\\Dcolb\\Documents\\NetBeansProjects\\Atari-Breakout-Group-Project-\\Atari Breakout Project\\src\\AtariBreakout\\Green.png"));
+             Rectangles[i].setSize(75, 50);
+             Rectangles[i].setBorder(blackline);
+             GamePanel3.add(Rectangles[i]);
+         }
+         for (int i = 24; i<32; i++){
+             Rectangles[i] = new JLabel(new ImageIcon("C:\\Users\\Dcolb\\Documents\\NetBeansProjects\\Atari-Breakout-Group-Project-\\Atari Breakout Project\\src\\AtariBreakout\\Blue.png"));
+             Rectangles[i].setSize(75, 50);
+             Rectangles[i].setBorder(blackline);
+             GamePanel4.add(Rectangles[i]);
+         }
+         for (int i = 32; i<40; i++){
+             Rectangles[i] = new JLabel(new ImageIcon("C:\\Users\\Dcolb\\Documents\\NetBeansProjects\\Atari-Breakout-Group-Project-\\Atari Breakout Project\\src\\AtariBreakout\\Purple.png"));
+             Rectangles[i].setSize(75, 50);
+             Rectangles[i].setBorder(blackline);
+             GamePanel5.add(Rectangles[i]);
+         }
+         GamePanel.add(GamePanel1);
+         GamePanel.add(GamePanel2);
+         GamePanel.add(GamePanel3);
+         GamePanel.add(GamePanel4);
+         GamePanel.add(GamePanel5);
+         GamePanel.add(PlayArea);
+         GamePanel.add(PlayArea);
+         GamePanel.add(PlayArea);
+         GamePanel.add(PlayArea);
+         GamePanel.add(PlayArea);
+         this.remove(panel1);
+         this.add(GamePanel);
     }
    
     
